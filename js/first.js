0c880b329dd54fea36e3f6b2ac33ff3dfaa3a804
Nikita <53258438+kazinak007@users.noreply.github.com>
2020-05-13 19:40:47 +0300

    DZ
    DZ

---
+++ js/first.js
@@ -0,0 +1,4 @@
+'use strict'
+let tCels = +prompt("Введите T по Целсию: ");
+let tFar = ((9/5)*tCels + 32);
+alert("Температура по Фаренгейту - " + tFar);
---
+++ js/second.js
@@ -0,0 +1,4 @@
+'use strict'
+let name = "Василий";
+let admin = name;
+alert(admin);
