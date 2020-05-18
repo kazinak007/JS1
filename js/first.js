6aeeb61144752b66edfcde1c825f66591c3089d1
Nikita <53258438+kazinak007@users.noreply.github.com>
2020-05-18 15:53:18 +0300

    ДЗ 2
    ДЗ 2 без 6** задания

--- js/first.js
+++
@@ -1,4 +0,0 @@
-'use strict'
-let tCels = +prompt("Введите T по Целсию: ");
-let tFar = ((9/5)*tCels + 32);
-alert("Температура по Фаренгейту - " + tFar);
---
+++ js/fourth.js
@@ -0,0 +1,39 @@
+'use strict'
+let number1 = 5;
+let number2 = 4;
+/**
+ * Сложение
+ * @param {number} number1 
+ * @param {number} number2 
+ * @returns {number}
+ */
+function plus(number1, number2) {
+    return number1 + number2;
+}
+/**
+ * Вычитание
+ * @param {*} number1 
+ * @param {*} number2 
+ * @returns {number}
+ */
+function minus(number1, number2) {
+    return number1 - number2;
+}
+/**
+ * Умножение
+ * @param {*} number1 
+ * @param {*} number2 
+ * @returns {number}
+ */
+function multiply(number1, number2) {
+    return number1 * number2;
+}
+/**
+ * Деление
+ * @param {*} number1 
+ * @param {*} number2 
+ * @returns {number}
+ */
+function divide(number1, number2) {
+    return number1 / number2;
+}
